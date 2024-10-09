cb999a325d32bf65c392c845317b6e42e5729196
kay-sb <kianoushsabouri@gmail.com>
2024-10-09 14:19:32 +0330

    Beto che

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import Navbar from './components/navbar';
-import Header from './components/header';
-import Footer from './components/footer';
-import { TextParallaxContentExample } from './components/cart';
-import Home from './pages/home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Sidebar from "./components/Sidebar";
+import { TextParallaxContentExample } from "./pages/Home";
+import Sales from "./pages/Sales";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <main >
-          <Header />
-          <TextParallaxContentExample />
+      <div className="App flex">
+        <Sidebar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<TextParallaxContentExample />} />
+            <Route path="/Sales" element={<Sales />} />
+          </Routes>
         </main>
-        <Footer />
       </div>
     </Router>
   );
---
+++ src/pages/Sales.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sales() {
+  return (
+    <div>
+      <h2 className="text-xl">Sales Page</h2>
+      <p>This is the Sales page where you can see your sales data.</p>
+    </div>
+  );
+}
+
+export default Sales;
